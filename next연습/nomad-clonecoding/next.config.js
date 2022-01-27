6345cf3dd948b51ab29369b830cb45d5439463e7
ByunMooYoung <mk@mklc.co.kr>
2022-01-27 13:12:44 +0900

    2022-01-27 nomad SSR까지

--- next연습/nomad-clonecoding/next.config.js
+++ next연습/nomad-clonecoding/next.config.js
@@ -1,3 +1,28 @@
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
+// redirect
+// source : 유저가 이동하려고 하는 위치
+// destination: 우리가 유저를 보낼 위치
+// permanent : 브라우저나 검색엔진이 이 정보를 기억하는지의 여부 설정
+// source의 :path 자리에 어떤 걸 넣어도 destination의 :path 자리에 똑같이 적용됨
+// path뒤에 *을 붙이면 /sdf/sdf/sdff 이런식으로 계속 해도 destination에 똑같이 적용됨
--- next연습/nomad-clonecoding/pages/index.js
+++ next연습/nomad-clonecoding/pages/index.js
@@ -2,25 +2,18 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Seo from "../conponents/Seo";
 
-const API_KEY = "9681b24cba137dbc5fc9d831319eb1d5";
-
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
+  // const [movies, setMovies] = useState([]);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { results } = await (await fetch("/api/movies")).json();
+  //     setMovies(results);
+  //   })();
+  // }, []);
   return (
     <div>
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div key={movie.id}>
           <h4>{movie.original_title}</h4>
         </div>
@@ -37,3 +30,18 @@ export default function Home() {
     </div>
   );
 }
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3002/api/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
+}
+// getServerSideProps는 이름 절대로 바꾸면 안됨
+// props : key or property
+// 이건 서버에서 돌아감
+// 여기서 무엇을 return 하던지, 이걸 props로써 page에게 줌
