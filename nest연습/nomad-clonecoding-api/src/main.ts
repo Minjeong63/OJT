e82da685101253d2d1c6ea0c9a8b8c874f0bc885
ByunMooYoung <mk@mklc.co.kr>
2022-01-28 10:30:47 +0900

    2022-01-28 #2.6까지

--- nest연습/nomad-clonecoding-api/src/main.ts
+++ nest연습/nomad-clonecoding-api/src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+    // whitelist : true로 설정하면 유효성 검사기는 validation decorator(IsString, IsNumber etc)를 사용하지 않는 속성의 유효성 검사 (반환 된) 개체를 제거
+    // forbidNonWhitelisted : true로 설정하면 화이트리스트에 없는 property validator를 제거하는 대신 예외가 발생
+    // transform : 유저들이 보낸 거를 우리가 원하는 실제 타입으로 변환해줌
+  )
   await app.listen(9102);
 }
 bootstrap();
--- nest연습/nomad-clonecoding-api/src/movies/movies.controller.ts
+++ nest연습/nomad-clonecoding-api/src/movies/movies.controller.ts
@@ -1,3 +1,5 @@
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
@@ -14,28 +16,26 @@ export class MoviesController {
     }
 
     @Get("/:id")
-    getOne(@Param('id') id: string): Movie {
+    getOne(@Param('id') id: number): Movie {
+        console.log(typeof id)
         return this.moviesService.getOne(id);
     }
     // url에 있는 id라는 parameter를 get하기 위해 @Param('id')라고 함
     // id라는 parameter를 id라는 argument에 저장하고 싶음
 
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this.moviesService.create(movieData);
     }
 
     @Delete("/:id")
-    remove(@Param('id') movieId: string) {
+    remove(@Param('id') movieId: number) {
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch("/:id")
-    patch(@Param('id') movieId: string, @Body() updateData) {
-        return {
-            updatedMovie: movieId,
-            ...updateData
-        }
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+        this.moviesService.update(movieId, updateData);
     }
 
 
